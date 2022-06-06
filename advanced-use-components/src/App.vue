30271281e47cdfc621793535e966771284c50b51
fco-junior <cassemirojunior16@gmail.com>
2022-06-06 12:57:28 -0300

    using dynamic component to render either Quotes or About component

--- advanced-use-components/src/App.vue
+++ advanced-use-components/src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="#app">
-    <Quotes />
-    <About />
+    <div>
+      <Button label="Quotes" class="p-button-lg" @click="currentComponent = 'Quotes'" />
+      <Button label="About" class="p-button-lg" @click="currentComponent = 'About'" />
+    </div>
+    <KeepAlive>
+      <component :is="currentComponent" />
+    </KeepAlive>
   </div>
 </template>
 
 <script>
 import Quotes from "./components/Quotes";
 import About from "./components/About";
-import './assets/scss/_app.scss'
+import "./assets/scss/_app.scss";
 
 export default {
-  name: 'App',
-  components: { Quotes, About }
-}
+  name: "App",
+  components: { Quotes, About },
+  data() {
+    return {
+      currentComponent: "Quotes",
+    };
+  },
+};
 </script>
-
