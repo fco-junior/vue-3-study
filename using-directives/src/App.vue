1c6ff17de5f5be697c9ad3b862ed93ac408d25cf
fco-junior <cassemirojunior16@gmail.com>
2022-06-08 09:33:19 -0300

    using delay modifier in color directive to apply delay in action of the directive

--- using-directives/src/App.vue
+++ using-directives/src/App.vue
@@ -2,6 +2,7 @@
   <div id="#app">
     <h1 v-color="color"> Using Directives </h1>
     <h1 v-color:background="'red'"> Using Directives and Args </h1>
+    <h1 v-color:background.delay="'green'"> Using Directives and Args and Modifiers </h1>
   </div>
 </template>
 
--- using-directives/src/main.js
+++ using-directives/src/main.js
@@ -4,12 +4,17 @@ import App from './App.vue';
 const app = createApp(App);
 
 app.directive('color', (el, binding) => {
-    
-  if(binding.arg === 'background')
-    el.style.backgroundColor = binding.value;
-  else
-    el.style.color = binding.value;
-
+  
+  let delay = 0
+  if(binding.modifiers['delay']) delay = 2000
+  
+  setTimeout( () => {
+    if(binding.arg === 'background')
+      el.style.backgroundColor = binding.value;
+    else
+      el.style.color = binding.value;
+  }, delay)
+  
 })
 
 app.mount('#app');
